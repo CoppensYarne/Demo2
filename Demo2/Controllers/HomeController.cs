cd17291cac379c056ac304ae43fe4c15c328590f
Yarne Coppens <yarne.coppens@student.howest.be>
2018-02-19 11:57:55 +0100

    Did something

---
+++ Demo2/Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Demo2.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        private string[] groenten = { "Rode kool", "Spruitjes", "Wortel", "Spinazie" };
+
+        public ViewResult Index(string name)
+        {
+            ViewBag.Groet = DateTime.Now.Hour < 12 ? "Goeiemorgen" : "Goeienamiddag";
+            return View();
+        }
+
+        public ViewResult Groenten(string zoekGroente)
+        {
+            ViewBag.Groenten = groenten;
+            if (!string.IsNullOrEmpty(zoekGroente) && Array.IndexOf(groenten, zoekGroente) != -1)
+            {
+                ViewBag.Zoekresultaat = 
+                $"De gezochte groente is de " +
+                $"{Array.IndexOf(groenten, zoekGroente) + 1}" +
+                $"e uit de lijst";
+            }
+            
+            return View();
+        }
+
+    }
+}
